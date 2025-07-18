21d8759d07e468dece4d82de0a9d92d002b2f4e2
mponiente <michaelwrites.biz@gmail.com>
2025-07-17 21:17:41 -0700

    swapped logo

--- frontend/src/components/HomeButton.tsx
+++ frontend/src/components/HomeButton.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import gtLogo from '../assets/icons/gt_regular.png';
+import gtLogo from '../assets/icons/gt_steam.png';
 
 const HomeButton = () => {
     return (
