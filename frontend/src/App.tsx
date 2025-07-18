08c771ee0c8743d545c34ca7e63ccf336a428218
mponiente <michaelwrites.biz@gmail.com>
2025-07-17 19:27:22 -0700

    organized

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import About from './pages/About'
-import Community from './pages/Community'
-import Contact from './pages/Contact'
-import Home from './pages/Home'
-import Shop from './pages/Shop'
-import Team from './pages/Team'
-import Footer from './components/Footer'
-import HomeButton from './components/HomeButton'
-import Navbar from './components/Navbar'
-import NavButton from './components/NavButton'
-
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import About from './pages/About';
+import Community from './pages/Community';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Team from './pages/Team';
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col bg-laurel text-fog">
+      <div className="min-h-screen flex flex-col bg-laurel text-cream">
         <Navbar />
 
-        <div className="flex justify-center space-x-4 mt-4">
-          <HomeButton />
-          <NavButton to="/about" label="About" />
-          <NavButton to="/community" label="Community" />
-          <NavButton to="/contact" label="Contact" />
-          <NavButton to="/shop" label="Shop" />
-          <NavButton to="/team" label="Team" />
-        </div>
-        
         <main className="flex-grow">
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/community" element={<Community />} />
             <Route path="/contact" element={<Contact />} />
@@ -42,4 +31,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
