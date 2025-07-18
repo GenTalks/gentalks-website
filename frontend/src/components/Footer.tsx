d324aebc0cb0976429aa227a0f42d3174d1f4f7c
mponiente <michaelwrites.biz@gmail.com>
2025-07-18 01:31:04 -0700

    home page? gnarly

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="text-center p-4 border-t font-teachers">
+    <footer className="text-center p-4 font-teachers">
       © 2025 GenTalks. All rights reserved.
     </footer>
   );
