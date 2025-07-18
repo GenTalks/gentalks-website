5ebe75ba0ce8d4995547256cd0325cba3fe2e126
mponiente <michaelwrites.biz@gmail.com>
2025-07-18 00:34:33 -0700

    replaced with mentors page

---
+++ frontend/src/pages/Mentors.tsx
@@ -0,0 +1,10 @@
+const Mentors = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-teachers">Mentors</h1>
+      <p>Coming soon!</p>
+    </div>
+  );
+};
+
+export default Mentors;
