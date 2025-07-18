2c0a41ac87ed3eebcaf328a00c5fb94e8f3a9443
mponiente <michaelwrites.biz@gmail.com>
2025-07-17 20:40:17 -0700

    Added gt icon

--- frontend/src/components/HomeButton.tsx
+++ frontend/src/components/HomeButton.tsx
@@ -1,2 +1,14 @@
-const HomeButton = () => <div>Home</div>;
+import { Link } from 'react-router-dom';
+
+const HomeButton = () => {
+    return (
+        <Link to="/">
+        <img
+          src="gt_regular.png"  
+          alt="GenTalks Home"
+          className="h-10 w-10 rounded-full hover:opacity-80 transition duration-200"
+        />
+      </Link>
+    )
+};
 export default HomeButton;
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,9 +3,8 @@ import NavButton from './NavButton';
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between px-6 py-4 bg-white shadow-md">
+    <nav className="flex items-center justify-between px-6 py-4 bg-cream shadow-md">
       <HomeButton />
-
       <div className="flex space-x-4">
         <NavButton to="/about" label="About" />
         <NavButton to="/community" label="Community" />
