666b27b31a1d00ebd1fd6671a5018e840219eeb7
mponiente <michaelwrites.biz@gmail.com>
2025-07-17 18:17:34 -0700

    Fixed NavButton

--- frontend/src/components/NavButton.tsx
+++ frontend/src/components/NavButton.tsx
@@ -1,1 +1,13 @@
-export default Navbutton;
+import { Link } from 'react-router-dom'
+
+const NavButton = ({ to, label }: { to: string; label: string }) => {
+  return (
+    <Link to={to}>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+            {label}
+        </button>
+    </Link>
+  );
+};
+
+export default NavButton;
