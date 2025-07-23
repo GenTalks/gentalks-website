dfe3af11af83f1e0a3094325f5d3e6493c9e890a
mponiente <michaelwrites.biz@gmail.com>
2025-07-22 22:34:37 -0700

    buh huh huh

--- frontend/src/pages/FAQs.tsx
+++ frontend/src/pages/FAQs.tsx
@@ -1,15 +1,68 @@
 import DiscordBanner from '../components/DiscordBanner';
+import FAQComp from '../components/FAQComp';
 
 const FAQs = () => {
   return (
     <div>
-      <section className="w-full flex flex-col items-center py-8 space-y-6 tracking-wide bg-cream text-fog mb-12">
+      <section className="w-full flex flex-col items-center py-8 space-y-6 tracking-wide bg-cream text-fog mb-4">
         <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-5xl transform font-bosk">
           Frequently Asked Questions
         </h1>
-        <p className="text-xl font-teachers">
-          the questions your pet asks when you're not home
+        <p className="text-xl font-teachers p-4">
+          the questions your dog thinks about in the shower when you're not home
         </p>
+      </section>
+
+      <section className="w-2/3 flex flex-col py-8 space-y-6 tracking-wide bg-cream text-fog mb-4 mx-auto">
+        <h1 className="font-teachers font-bold text-5xl text-left">
+          General Questions
+        </h1>
+
+        <div className="font-teachers font-normal text-2xl text-left transform -translate-x-40">
+          <FAQComp question="What is GenTalks?" answer="GenTalks is a nonprofit organization committed to empowering the high school community with nurturing conversations about personal and academic growth. Through virtual mentorship calls, we help students get in touch with mentors." />
+          <FAQComp question="Is GenTalks free?" answer="um *looks both ways* yes your honor *gulp* GenTalks will NEVER charge for our services. EVER. Pinky promise." />
+          <FAQComp question="Can I still be a community member even if I'm not in high school?" answer="erm i actually dunno im just the web dev chat" />
+
+        </div>
+      </section>
+
+      <section className="w-2/3 flex flex-col py-8 space-y-6 tracking-wide bg-cream text-fog mb-4 mx-auto">
+        <h1 className="font-teachers font-bold text-5xl text-left">
+          Students
+        </h1>
+
+        <div className="font-teachers font-normal text-2xl text-left transform -translate-x-40">
+          <FAQComp question="What is GenTalks?" answer="GenTalks is a nonprofit organization committed to empowering the high school community with nurturing conversations about personal and academic growth. Through virtual mentorship calls, we help students get in touch with mentors." />
+          <FAQComp question="Is GenTalks free?" answer="um *looks both ways* yes your honor *gulp*" />
+          <FAQComp question="What is GenTalks?" answer="idk i swear i just told ya but my memory has been erm escaping a little faster today" />
+
+        </div>
+      </section>
+
+      <section className="w-2/3 flex flex-col py-8 space-y-6 tracking-wide bg-cream text-fog mb-4 mx-auto">
+        <h1 className="font-teachers font-bold text-5xl text-left">
+          Mentors
+        </h1>
+
+        <div className="font-teachers font-normal text-2xl text-left transform -translate-x-40">
+          <FAQComp question="What is GenTalks?" answer="GenTalks is a nonprofit organization committed to empowering the high school community with nurturing conversations about personal and academic growth. Through virtual mentorship calls, we help students get in touch with mentors." />
+          <FAQComp question="Is GenTalks free?" answer="um *looks both ways* yes your honor *gulp* " />
+          <FAQComp question="What is GenTalks?" answer="idk i swear i just told ya but my memory has been erm escaping a little faster today" />
+
+        </div>
+      </section>
+
+      <section className="w-2/3 flex flex-col py-8 space-y-6 tracking-wide bg-cream text-fog mb-16 mx-auto">
+        <h1 className="font-teachers font-bold text-5xl text-left">
+          General Questions
+        </h1>
+
+        <div className="font-teachers font-normal text-2xl text-left transform -translate-x-40">
+          <FAQComp question="What is GenTalks?" answer="GenTalks is a nonprofit organization committed to empowering the high school community with nurturing conversations about personal and academic growth. Through virtual mentorship calls, we help students get in touch with mentors." />
+          <FAQComp question="Is GenTalks free?" answer="um *looks both ways* yes your honor *gulp*" />
+          <FAQComp question="What is GenTalks?" answer="idk i swear i just told ya but my memory has been erm escaping a little faster today" />
+
+        </div>
       </section>
 
       <section className="w-full flex flex-col items-center pt-12 space-y-6 tracking-wide bg-laurel">
