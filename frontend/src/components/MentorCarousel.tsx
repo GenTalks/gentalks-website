f307b7bec93ecde2859e24833c0961936fdba42e
mponiente <michaelwrites.biz@gmail.com>
2025-07-28 00:40:08 -0700

    gonna go experiment hehehe

--- frontend/src/components/MentorCarousel.tsx
+++ frontend/src/components/MentorCarousel.tsx
@@ -1,79 +1,47 @@
 import { useState, useEffect, useRef } from 'react';
 import MentorCard from './MentorCard';
 import NavButton from './NavButton';
+import { sanityClient } from '../lib/sanityClient';
+
+// Reuse the MentorCardProps type for consistency
+interface Mentor {
+  image: string;
+  name: string;
+  title: string;
+}
 
 const MentorCarousel = () => {
+  const [mentors, setMentors] = useState<Mentor[]>([]);
   const [translateX, setTranslateX] = useState(0);
   const animationRef = useRef<number | null>(null);
-
-  const mentors = [
-    {
-      image: "https://staticg.sportskeeda.com/editor/2021/11/c8fcd-16372673354076-1920.jpg",
-      name: "Albedo",
-      title: "Biochemistry & Synthetic Biology Student - Oxford University"
-    },
-    {
-      image: "https://s1.zerochan.net/Alhaitham.600.3894482.jpg",
-      name: "Alhaitham",
-      title: "Applied Mathematics & Cognitive Science Student - University of Cambridge"
-    },
-    {
-      image: "https://i.pinimg.com/736x/2e/4f/8e/2e4f8ee4d40dd70c65e033feca308bca.jpg",
-      name: "Baizhu",
-      title: "Traditional Chinese Medicine Student - Beijing University of Chinese Medicine"
-    },
-    {
-      image: "https://s1.zerochan.net/Clorinde.600.4001291.jpg",
-      name: "Clorinde",
-      title: "Law Student - Sciences Po"
-    },
-    {
-      image: "https://i.pinimg.com/originals/39/7c/dc/397cdcabea519f46a9e440136fb2479f.jpg",
-      name: "Jean",
-      title: "Political Science Student - John Hopkins University"
-    },
-    {
-      image: "https://i.pinimg.com/originals/a8/ad/dd/a8addd136eb7d85cd12c7936d3b53088.jpg",
-      name: "Kamisato Ayaka",
-      title: "International Relations & Classical Literature Student - Kyoto University"
-    },
-    {
-      image: "https://wallpapercave.com/wp/wp11127199.jpg",
-      name: "Kamisato Ayato",
-      title: "Political Science Student - Harvard University",
-    },
-    {
-      image: "https://images4.alphacoders.com/110/thumb-1920-1109283.jpg",
-      name: "Mona",
-      title: "Astrophysics Student - University of Cambridge",
-    },
-    {
-      image: "https://avatarfiles.alphacoders.com/334/334716.jpg",
-      name: "Nahida",
-      title: "Environmental Science & Linguistics Student - UC Berkeley",
-    },
-    {
-      image: "https://images2.alphacoders.com/133/thumb-1920-1334943.jpeg",
-      name: "Neuvilette",
-      title: "Law Student - Yale University",
-    },
-    {
-      image: "https://th.bing.com/th/id/R.39c314f14ff5a096f98b97abf1711d1d?rik=AyTG3llOfGn6Ug&pid=ImgRaw&r=0",
-      name: "Raiden Shogun",
-      title: "Philosophy Student - University of Tokyo",
-    },
-    {
-      image: "https://i.pinimg.com/originals/8c/1c/f6/8c1cf65f7d700e8a4fbe0cbb32c4b996.jpg",
-      name: "Kaedehara Kazuha",
-      title: "Environmental Studies & World Literature Student - Reed College",
-    },
-  ];
-
-  const extendedMentors = [...mentors, ...mentors];
   const cardWidth = 280;
-  const totalExtendedWidth = extendedMentors.length * cardWidth;
 
   useEffect(() => {
+    const fetchMentors = async () => {
+      const data = await sanityClient.fetch(`
+        *[_type == "mentorpage"]{
+          mentorImage,
+          mentorname,
+          role
+        }
+      `);
+
+      const formatted: Mentor[] = data.map((m: any) => ({
+        image: m.mentorImage,
+        name: m.mentorname,
+        title: m.role,
+      }));
+
+      // ✅ This will no longer be underlined in red
+      setMentors([...formatted, ...formatted]);
+    };
+
+    fetchMentors();
+  }, []);
+
+  useEffect(() => {
+    const totalExtendedWidth = mentors.length * cardWidth;
+
     const animate = () => {
       setTranslateX(prev => {
         const newX = prev - 1.5;
@@ -89,7 +57,7 @@ const MentorCarousel = () => {
         cancelAnimationFrame(animationRef.current);
       }
     };
-  }, [totalExtendedWidth]);
+  }, [mentors]);
 
   return (
     <div className="relative w-full mb-[1.5rem]">
@@ -104,12 +72,13 @@ const MentorCarousel = () => {
           className="border-laurel border-2 bg-cream text-laurel px-6 py-2 rounded-full hover:text-cream hover:bg-laurel hover:border-cream hover:border-2"
         />
       </div>
+
       <div className="relative overflow-hidden w-full mt-20">
         <div
           className="flex"
           style={{ transform: `translateX(${translateX}px)` }}
         >
-          {extendedMentors.map((mentor, index) => (
+          {mentors.map((mentor, index) => (
             <MentorCard
               key={`mentor-${index}`}
               image={mentor.image}
