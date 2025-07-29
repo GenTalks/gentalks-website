63f38e027d9fd515132ac593fd80705d859c86be
Dylan Tran <dylanbt928@gmail.com>
2025-07-29 16:47:47 -0700

    remove border

--- frontend/src/components/DiscordBanner.tsx
+++ frontend/src/components/DiscordBanner.tsx
@@ -3,7 +3,7 @@ import DiscordButton from "../components/DiscordButton";
 
 const JoinDiscordSection = () => {
   return (
-    <section className="border-4 border-cream bg-laurel text-cream py-16 px-8 rounded-3xl max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-between gap-12 shadow-lg flex-wrap">
+    <section className="border-0 border-cream bg-laurel text-cream py-16 px-8 rounded-3xl max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-between gap-12 shadow-lg flex-wrap">
       <div className="flex-shrink-0">
         <img
           src={gtLogo}
@@ -15,7 +15,8 @@ const JoinDiscordSection = () => {
       <div className="flex flex-col items-center lg:items-start gap-6 max-w-3xl text-center lg:text-left">
         <h2 className="text-4xl md:text-5xl font-bosk">Join Gentalks!</h2>
         <p className="text-lg md:text-xl leading-relaxed font-teachers">
-          an online space for growth, where members uplift each other and share valuable insights on careers and life.
+          an online space for growth, where members uplift each other and share
+          valuable insights on careers and life.
         </p>
         <DiscordButton />
       </div>
