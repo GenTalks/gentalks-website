b27f78a2f1a28025863619936d94a11e9d225685
Dylan Tran <dylanbt928@gmail.com>
2025-08-01 13:12:00 -0700

    remove navbar button

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -29,17 +29,7 @@ export default function Navbar() {
           <NavButton to="/faqs" label="FAQs" />
           <NavButton to="/contact-us" label="Contact" />
         </div>
-
-        <div>
-          <NavButton
-            to="/book-a-mentor"
-            label="Book a mentor"
-            className="bg-cream text-cream px-6 py-2 rounded-full"
-            disableHover
-          />
-        </div>
       </div>
-
 
       <button
         className="2xl:hidden p-2 rounded-md focus:outline-none"
@@ -67,13 +57,8 @@ export default function Navbar() {
             to="/community"
             label="Community"
             onClick={() => setOpen(false)}
-            
           />
-          <NavButton
-            to="/about"
-            label="About"
-            onClick={() => setOpen(false)}
-          />
+          <NavButton to="/about" label="About" onClick={() => setOpen(false)} />
           <NavButton
             to="/content"
             label="Content"
@@ -89,13 +74,6 @@ export default function Navbar() {
             to="/contact-us"
             label="Contact"
             onClick={() => setOpen(false)}
-          />
-          <NavButton
-            to="/book-a-mentor"
-            label="Book a mentor"
-            onClick={() => setOpen(false)}
-            className="bg-laurel text-cream px-6 py-2 rounded-full hover:bg-basil"
-            disableHover
           />
         </div>
       )}
